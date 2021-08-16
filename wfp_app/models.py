5f71fdd9a5ea39042613ac560869ce905bf8ea47
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-16 21:05:10 +0300

    Add user model and admin module

--- wfp_app/models.py
+++ wfp_app/models.py
@@ -1,10 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db import IntegrityError
+from django.core.validators import MaxLengthValidator,MinLengthValidator
 from django.db.models.deletion import CASCADE
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True)
+    first_name =models.CharField(max_length=144,null=True,blank=True)
+    last_name = models.CharField(max_length=144,null=True,blank=True)
+
+
+    def delete_user(self):
+        self.delete()
 
 # Create your models here.
 class Country(models.Model):
   iso_code = models.CharField(max_length=20)
   name = models.CharField(max_length=30)
+
+  def save_country(self):
+    self.save()
+
+  def delete_country(self):
+    self.delete()
+
+  def __str__(self):
+    return self.name
 
 class Intervention(models.Model):
   office_id = models.ForeignKey(Country,on_delete=CASCADE)
@@ -12,3 +35,12 @@ class Intervention(models.Model):
   code_name = models.CharField(max_length=20)
   start_date = models.DateTimeField()
   end_date = models.DateTimeField()
+
+  def save_office(self):
+    self.save()
+
+  def delete_office(self):
+    self.delete()
+
+  def __str__(self):
+    return self.name
