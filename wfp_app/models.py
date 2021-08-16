5d37587aed26f3ee54fe398c7621b00d85e4068d
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-16 20:44:58 +0300

    Create models

--- wfp_app/models.py
+++ wfp_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Country(models.Model):
+  iso_code = models.CharField(max_length=20)
+  name = models.CharField(max_length=30)
+
+class Intervention(models.Model):
+  office_id = models.ForeignKey(Country,on_delete=CASCADE)
+  name = models.CharField(max_length=30)
+  code_name = models.CharField(max_length=20)
+  start_date = models.DateTimeField()
+  end_date = models.DateTimeField()
